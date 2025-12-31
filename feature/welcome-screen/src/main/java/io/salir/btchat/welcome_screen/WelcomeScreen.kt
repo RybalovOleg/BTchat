a7265ddf1ceca592fe180fb427956f48b77952ef
Oleg <rybalov.oleg.dev@gmail.com>
2025-12-31 18:20:32 +0300

    feat(welcome-screen): Шаблон WelcomeScreen

--- feature/welcome-screen/src/main/java/io/salir/btchat/welcome_screen/WelcomeScreen.kt
+++ feature/welcome-screen/src/main/java/io/salir/btchat/welcome_screen/WelcomeScreen.kt
@@ -1,15 +1,23 @@
 package io.salir.btchat.welcome_screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,7 +42,25 @@ private fun WelcomeScreenContent() {
             Column(
                 modifier = Modifier.fillMaxSize()
             ) {
+                Box(
+                    modifier = Modifier.weight(2f)
+                ) { }
+                Column(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {}
+                    ) { Text("Host new chat") }
 
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {}
+                    ) { Text("Join to chat") }
+                }
             }
         }
     }
