5fa9fa9d93a6deb082fb2f56aeab32dc42ceee02
Oleg <rybalov.oleg.dev@gmail.com>
2026-01-25 18:36:33 +0300

    feat(common): SimpleDataResult<T>, объединение файлов, модификатор out для типа в WithData

--- core/common/src/main/java/io/salir/btchat/core/common/Result.kt
+++ core/common/src/main/java/io/salir/btchat/core/common/Result.kt
@@ -1,12 +1,20 @@
 package io.salir.btchat.core.common
 
-sealed class Result<out T> {
+sealed class Result<out T, out P: Progress> {
 
-    object Empty : Result<Nothing>()
+    object Empty : Result<Nothing, Nothing>()
 
-    data class Loading(val progress: Progress = Progress.Unspecified) : Result<Nothing>()
+    data class Loading<out P: Progress>(val progress: P) : Result<Nothing, P>() {
+        companion object {
+            val Unspecified = Loading(Progress.Unspecified)
+        }
+    }
 
-    data class Success<out T>(val data: T) : Result<T>()
+    data class Success<out T>(val data: T) : Result<T, Nothing>()
 
-    data class Error(val exception: Exception) : Result<Nothing>()
+    data class Error(val exception: Exception) : Result<Nothing, Nothing>()
 }
+
+typealias ListResult<T> = Result<List<T>, Progress.WithData<List<T>>>
+
+typealias SimpleDataResult<T> = Result<T, Progress.Unspecified>
